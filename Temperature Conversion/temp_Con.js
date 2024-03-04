278f25f7724342b36d8537badc8c1f9bf9aaaa37
chathuraDeshan <chathuradeshan597@gmail.com>
2024-03-04 08:19:59 +0530

    add the fahrenheit to celsius formula

--- Temperature Conversion/temp_Con.js
+++ Temperature Conversion/temp_Con.js
@@ -12,10 +12,18 @@ function convert(){
     if(toFahrenheit.checked){
         temp = Number(textBox.value);
         temp = temp * 9 / 5 + 32;
-        result.textContent = temp + "℉";
+        result.textContent = temp.toFixed(1) + "℉";
+
     }
     
+    else if(toCelsius.checked){
+        temp = Number(textBox.value);
+        temp = (temp - 32) * (5/9) ;
+        result.textContent = temp.toFixed(1) + "℃";
+        
+    }
+
     else{
-        result.textContent = "Select a unit"
+        result.textContent = "Select a unit";
     }
 }
